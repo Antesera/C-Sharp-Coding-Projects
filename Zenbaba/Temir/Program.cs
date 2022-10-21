ce500a1e4ac68aa01cf06f6d65a14e27c2eb5ac5
Antesera <107664612+Antesera@users.noreply.github.com>
2022-10-21 09:21:12 -0400

    Add files via upload

---
+++ Zenbaba/Temir/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {         
+
+             string width = "10";
+			 int width = Convert.ToInt32(Console.ReadLine(); 
+             string length = "11";
+			 int length = Convert.ToInt32(Console.ReadLine(); 
+             string height = "12";
+			 int height = Convert.ToInt32(Console.ReadLine(); 
+             string weight = "40";
+			 int weight = Convert.ToInt32(Console.ReadLine(); 
+			 
+            //string PriceQuote = (height * width *  length * weight) = $ "528";
+			
+            string product = (" 12 * 10 * 11 * 40 ");
+            Console.WriteLine(product);
+			int product = convert.ToInt32(Console.Readline());
+			
+            string PriceQuote = ("12 * 10 * 11 * 40 / 100 ");
+            Console.WriteLine($"PriceQuote");
+			int priceQuote = convert.ToInt32(Console.Readline());
+			
+			if(string Weight > "50")
+			 {
+			 console.WriteLine("Package too heavy to be shipped via Package Express.Have a good day.");
+			 
+			 }
+			
+            Console.ReadLine();
+
+        }
+    }
+}
