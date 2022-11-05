6c54f4a9488e662d53e5cd8440d922a2c1776588
Antesera <107664612+Antesera@users.noreply.github.com>
2022-11-05 03:41:01 -0400

    Add files via upload

---
+++ List  of Array/List  of Array/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+                                       
+namespace List__of_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] cars = { "Toyota", "Hunda", "Ford", "Mazda" };
+              
+            Console.WriteLine("All the element of cars string is:\n\n");
+            int[] array1 = new int[] { 1, 3, 5, 7, 9 };   //array1 =  {1, 3, 5, 7, 9}; is Error
+            Console.WriteLine(cars[0]);
+
+            Console.WriteLine(array1[3]);
+        }
+    }
+}
+
+     
