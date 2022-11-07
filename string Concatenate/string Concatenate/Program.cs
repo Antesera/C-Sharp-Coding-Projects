8daf821b7360b3d55c9d27166c91b7abef2962a3
Antesera <107664612+Antesera@users.noreply.github.com>
2022-11-07 08:56:58 -0500

    Add files via upload

---
+++ string Concatenate/string Concatenate/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace string_Concatenate
+{
+    class Program
+    {
+        private static IEnumerable<int> uniqueNumbers;
+
+        private static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 4, 5, 6, 1, 2, 3, 2, 1, 0 };
+
+            Console.WriteLine("number of Array are below");
+            
+            //foreach
+            foreach (int num  in numbers)
+            {
+                //for
+                for (int i = 0; i < 9; i--)
+                {
+                    Console.Write("value : {0} ", i);
+                }
+                
+            }
+            for (int i = 0;  i < 9; i++) // some code which run infinite times
+            {
+                printf("[%d] : %d\n", i, numbers[0]);
+            }
+
+            for (int i in uniqueNumbers)
+            {
+                uniqueNumbers = numbers.Select(i => i).Distinct();
+            }
+
+            for (var i = 1; i <= 10; i++)
+            {
+                if (i == 5)
+                {
+                    break;
+                }
+                Console.WriteLine($"Our current number is: {i}");
+            }
+
+            //while           
+            int i = 0;
+            while (i <= 10)
+            {
+                i++;
+                if (i % 3 != 0)
+                {
+                    continue;
+                }
+                Console.WriteLine($"{i} is a multiple of 3");
+            }
+        }
+          private static void printf(string v1, int i, int v2)
+          {
+            throw new NotImplementedException();
+          }
+    }
+}
