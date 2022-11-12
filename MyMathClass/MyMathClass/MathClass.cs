2d07cfa06fc4053093f20d61f11e03f435650dc7
Antesera <107664612+Antesera@users.noreply.github.com>
2022-11-11 22:33:35 -0500

    Add files via upload

---
+++ MyMathClass/MyMathClass/MathClass.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyMathClass
+{
+    class MathClass
+    {
+        //private variables
+        private int _firstNumber;
+        private int _secondNumber;
+         //Constructor first number and second number
+        public MathClass(int firstNumber, int secondNumber)
+        {
+            _firstNumber = firstNumber;
+            _secondNumber = secondNumber;
+        }
+         //Default Constructor
+        public MathClass()
+        {
+            _firstNumber = 10;
+            _secondNumber = 5;
+        }
+         //property to Set and Get _firstNumber 
+        public int FirstNumber
+        {
+            set
+            {
+
+            }
+            get
+            {
+                return _firstNumber;
+            }
+        }
+
+        public int SecondNumber
+        {
+            set
+            {
+                _secondNumber = value;
+            }
+            get
+            {
+                return _secondNumber;
+            }
+        }
+        //Calls of multiple 
+        public int Multiple()
+        {
+            return _firstNumber * _secondNumber;
+        }
+
+        public int Divide()
+        {
+            if (_firstNumber == 0)
+                return 0;
+
+            else
+            return _firstNumber / _secondNumber;
+
+        }
+    }
+}
---
+++ MyMathClass/MyMathClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyMathClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathClass myClass = new MathClass();
+            
+            myClass.FirstNumber = 10;
+            myClass.SecondNumber = 5;
+
+            Console.WriteLine(myClass.Multiple().ToString());
+
+            Console.WriteLine("Divide Result: " + myClass.Divide());
+
+            Console.ReadLine();
+        }
+    }
+}
