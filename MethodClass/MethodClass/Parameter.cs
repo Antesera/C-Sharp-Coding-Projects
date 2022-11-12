d488b2496d601efbd61e1c90fff1063c97c8920e
Antesera <107664612+Antesera@users.noreply.github.com>
2022-11-12 12:43:54 -0500

    Add files via upload

---
+++ MethodClass/MethodClass/Parameter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class Parameter
+    {
+        private int Value1 { get; set; }
+        private int value2 { get; set; }
+
+         //declarative statement of the method
+        public void MethodClass()
+        {
+            Console.WriteLine(addTwoIntegers(this.Value1, value2));
+            Console.ReadLine();
+        }
+        public int MethodClass(int value1, int value2)
+        {
+            return addTwoIntegers(value1, value2);
+        }
+
+        //create two or more methods of the same name but difference signiture
+        public int MethodClass(int value1, int value2, int maxTotal)
+        {
+            int total = addTwoIntegers(value1, value2);
+            if (total > maxTotal) total = maxTotal;
+            return total;
+        }
+        private int addTwoIntegers(int value1, int value2 = 0)
+        {
+            int sum = value1 + value2;
+            return sum;
+        }
+    }
+}
---
+++ MethodClass/MethodClass/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parameter MyObject = new Parameter();
+
+            //MyObject.value1 = 4;
+            //MyObject.value2 = 8;
+            //MyObject.MethodClass();
+            Console.WriteLine(MyObject.MethodClass(5, 9, 11));
+            Console.ReadLine();
+        }
+    }
+}
