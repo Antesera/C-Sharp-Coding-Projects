9147d594045b3761c27b8db79371f97386eb6d39
Antesera <107664612+Antesera@users.noreply.github.com>
2022-11-12 03:42:27 -0500

    Add files via upload

---
+++ ClassMethod/ClassMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethod
+{                  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassMethod.secondClass clMath = new ClassMethod.secondClass();
+
+            clMath.whoAmI("Ante");
+            
+            Console.WriteLine(clMath.addMe(3, 8));
+            Console.WriteLine(clMath.addMe(3, 8, 4));
+            Console.WriteLine(clMath.subMe(3, 8));
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ClassMethod/ClassMethod/secondClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethod
+{
+    class secondClass
+    {
+        //declaration of intialization
+        public void whoAmI(string name)
+        {
+            Console.WriteLine("Hello "  + name  +  ", how are you doing?");
+        }
+
+        // property to  add two numbers
+        public int addMe(int a, int b)
+        {
+            int totalValue = 0;
+            totalValue = (a + b);
+            return totalValue;
+        }
+
+        // property to  add three numbers
+        public int addMe(int a, int b, int c)
+        {
+            int totalValue;
+            Console.WriteLine("you are going to add 3 numbers");
+            totalValue = (a + b + c);
+
+            //double.totalValue = Add(1.0, +2.0 + 3.0);
+            Console.WriteLine("total value of the three " + "double value:" + totalValue );
+            return totalValue;
+        }
+
+        // property to  substract two numbers
+        public int subMe(int a, int b)
+        {
+            int totalValue = 0;
+            totalValue = (a - b);
+            return totalValue;
+        }
+    }
+}
