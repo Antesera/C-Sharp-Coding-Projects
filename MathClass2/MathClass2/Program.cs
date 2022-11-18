4f139837450f73c456f90c3a5783080fed4a327b
Antesera <107664612+Antesera@users.noreply.github.com>
2022-11-17 22:03:25 -0500

    Add files via upload

---
+++ MathClass2/MathClass2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClass2
+{
+    class Program
+    {
+        public int Data { get; set; }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter input:");
+            string Line = Console.ReadLine();
+
+            var separateObject = new Separate(); // you have to add reference, if is not
+            var result = SeparateObject.GetAdd(data);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MathClass2/MathClass2/Separete.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathClass2
+{
+    class Separete
+    {
+        public double GetAdd(int data)
+        {
+            data = data + 4;
+            return GetSubstract(data);
+        }
+
+        private double GetSubtract(double data)
+        {
+            data = data - 3;
+            return GetDivide(data);
+        }
+
+        private  double GetDivide(double data)
+        {
+            return data / 3;
+        }
+    }
+
+}
