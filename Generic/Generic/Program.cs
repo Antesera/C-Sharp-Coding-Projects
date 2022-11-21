12785340aa9a0826017efd2ff435546c5172da22
Antesera <107664612+Antesera@users.noreply.github.com>
2022-11-21 11:16:05 -0500

    Add files via upload

---
+++ Generic/Generic/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generic
+{
+    class Employee
+    {
+        //main void
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Thing.CompareValues (12, "Hello"));
+            Console.WriteLine(Thing.CompareTypes ("Hello", "Hello"));
+           
+            bool Equal = Thing.AreEqual(1, 1);
+             
+            if (Equal)
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+            {
+                Console.WriteLine("Not Equal");
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    //contractor
+    public class Thing
+    {
+        //type parameter using brackets
+        public static bool CompareValues<T01, T02>(T01 value01, T02 value02)
+        {
+            return value01.Equals(value02);
+        }
+        public static bool CompareTypes<T01, T02>(T01 type01, T02 type02)
+        {
+            return typeof(T01).Equals(typeof(T02));
+        }
+        public static bool AreEqual<T01, T02>(T01 value01, T02 value02)
+        {
+            return value01.Equals(value02);
+        }
+
+    }
+}
+
