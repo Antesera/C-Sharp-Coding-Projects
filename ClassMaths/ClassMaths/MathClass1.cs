cfbf128495e5bbc573d0da1833c58d95112ca507
Antesera <107664612+Antesera@users.noreply.github.com>
2022-11-21 17:39:18 -0500

    Add files via upload

---
+++ ClassMaths/ClassMaths/MathClass1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMaths
+{
+    class MathClass1
+    {
+    
+               //calls of multiple
+        public int multiple(int div)
+        {
+            return div*2;
+        }
+        //calls of addition
+        public int add(int div)
+        {
+            return div +2;
+        }
+        //calls of divide
+        public int Divide (int div )
+        {
+            
+                return div / 2;
+            
+        }
+    }
+}
---
+++ ClassMaths/ClassMaths/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMaths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathClass1 Class1 = new MathClass1();
+
+            
+            Console.WriteLine("please enter the number");
+
+            int div = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine(Class1.multiple(div).ToString());
+            Console.WriteLine(Class1.add(div).ToString());
+            Console.WriteLine("Divide Result: " + Class1.Divide(div));
+        }
+    }
+}
+
