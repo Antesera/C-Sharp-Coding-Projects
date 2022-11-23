62235c9b6b1a2f9dc57298e86361337feb9e2f95
Antesera <107664612+Antesera@users.noreply.github.com>
2022-11-23 17:28:38 -0500

    Add files via upload

---
+++ MainMethod1/MainMethod1/Sera.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MainMethod1
+{
+    public class Sera
+    {       
+        //Constructor the string
+        public void WhoAmI(string name)
+        {
+            Console.WriteLine("Hello" + name + ", How are doing ?");
+        }
+
+        //default Constractor
+        public int AddMe(decimal a)
+        {
+            int result = (Convert.ToInt32(a) + 8);
+            return result;
+        }
+        public int AddMe(int b)
+        {
+            int result = (Convert.ToInt32(b) + 4);
+            return result;
+        }
+        public int AddMe(double c)
+        {
+
+            int result = (Convert.ToInt32(c) + 3);
+            return result;
+        }
+
+       
+
+        
+        
+    }
+}
