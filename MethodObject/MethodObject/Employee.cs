e6a39b1f8c534b3fc4028fb96895e861d716673d
Antesera <107664612+Antesera@users.noreply.github.com>
2022-11-23 17:42:46 -0500

    Add files via upload

---
+++ MethodObject/MethodObject/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodObject
+{
+    public class Employee : Person
+    {
+        public int ID { get; set; }
+
+        public int Id()
+        {
+            return ID;
+        }
+    }
+}
