b6795d5deb4f9768f48d89e6ba0e56a430fe3343
Antesera <107664612+Antesera@users.noreply.github.com>
2022-11-22 20:32:22 -0500

    Add files via upload

---
+++ MainMethod1/MainMethod1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MainMethod1
+{
+    class Program
+    {
+        static void Main()
+        {
+            MainMethod1.Sera Math = new MainMethod1.Sera();
+
+            Math.WhoAmI("Ante");
+
+            Console.WriteLine(8 + 3);
+            Console.ReadLine();
+        }
+    }
+}
