21fba7418f04e2d68e9f6e25fa5e7f6db2d4689a
Antesera <107664612+Antesera@users.noreply.github.com>
2022-11-23 23:31:08 -0500

    Add files via upload

---
+++ MethodObject/MethodObject/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MethodObject
+{
+    class Program
+    {
+        public static int Id { get; set; }
+        public static string FirstName { get; set; }
+        public static string LastName { get; set; }
+
+        public static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.FirstName = "Dani";
+            person1.LastName = "Angu";
+
+            Employee employee = new Employee();
+            employee.ID = Id;
+            employee.FirstName = "Heni";
+            employee.LastName = "Sami";
+            employee.SayName();
+                
+            Console.ReadLine();
+        }
+    }
+}
