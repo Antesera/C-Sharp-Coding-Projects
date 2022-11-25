29bb2b4c267a392450455a1e8d58023e974d4f0e
Antesera <107664612+Antesera@users.noreply.github.com>
2022-11-25 00:52:49 -0500

    Add files via upload

---
+++ PolyInterface/PolyInterface/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PolyInterface
+{
+    class Program
+    {
+        interface IQuittable
+        {
+            void Quite();
+        }
+        class Employee :IQuittable
+        {
+            public void Quite()
+            {
+                Console.WriteLine("Employee have Quite mistake at work station");
+            }
+        }
+        class Salary : IQuittable
+        {
+            public void Quite()
+            {
+                Console.WriteLine("Employee was quite soon to get ovettime");
+            }
+        }
+        class Benefit : IQuittable
+        {
+            public void Quite()
+            {
+                Console.WriteLine("Employee was quite a sudden change by companys gift!");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.Quite();
+
+            Salary salary = new Salary();
+            salary.Quite();
+
+            Benefit benefit = new Benefit();
+            benefit.Quite();
+
+        }
+    }
+}
