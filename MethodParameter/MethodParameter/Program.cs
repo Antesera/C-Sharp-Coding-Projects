941e305caf2ec50d532d485951e8dd2e5a1b67a0
Antesera <107664612+Antesera@users.noreply.github.com>
2022-11-26 10:36:58 -0500

    Add files via upload

---
+++ MethodParameter/MethodParameter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MethodParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            parameter1 MyObject = new parameter1();
+            //MyObject.num1 = 4;
+            //MyObject.num2 = 8;
+            //MyObject.MethodParameter();
+            Console.WriteLine(MyObject.MethodParameter(5, 9, 11));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodParameter/MethodParameter/parameter1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MethodParameter
+{
+    class parameter1
+    {
+        private int num1 { get; set; }
+        private int num2 { get; set; }
+
+        //declarative statement of the method
+        public void MethodParameter1()
+        {
+            Console.WriteLine("Enter first number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number:");
+        }
+        public int MethodParameter1( int num1, int num2)
+        {
+            
+            try
+            {
+                addTwoIntegers(num1, num2);
+            }
+            catch
+            {
+                addTwoIntegers(num1);
+               
+            }
+
+            int result = addTwoIntegers(num1, num2);
+            return result;
+        }
+        
+        private int addTwoIntegers(int num1 ,int num2 = 0)
+        {
+            int sum = num1 + num2;
+            return sum;
+        }
+
+
+     }
+       
+}
