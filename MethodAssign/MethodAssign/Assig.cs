bd5c0271644329b070cf6bbf5018efe0e3ec3ec4
Antesera <107664612+Antesera@users.noreply.github.com>
2022-11-26 10:50:16 -0500

    Add files via upload

---
+++ MethodAssign/MethodAssign/Assig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodAssign
+{
+    class Assig
+    {
+        public int AddNumber(int firstNumber, params int[] numbers)
+        {
+            int temp = 0;
+            foreach (int tempNum in numbers)
+            {
+                temp += tempNum;
+            }
+            return temp + firstNumber;
+        }
+
+        public void MainMethod()
+        {
+            AddNumber(8);
+            AddNumber(8, 9);
+        }
+    }
+}
---
+++ MethodAssign/MethodAssign/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MethodAssign
+{
+    class Program
+    {
+        private static int temp;
+        public static int FirstNumber { get; set; }
+        public static int SecondNumber { get; set; }
+        private static int TempNum;
+
+        static void Main(string[] args)
+        {
+            MethodAssign.Assig X = new MethodAssign.Assig();
+            {
+                FirstNumber = FirstNumber;
+                TempNum = temp + FirstNumber;
+            }
+
+            Console.WriteLine( "Sum:" + "FirstNumber  + params int[] numbers");
+        }
+    }
+}
