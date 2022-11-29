2f3cfbb26e7a9a607fefdb6b98f8e2be02af83fb
Antesera <107664612+Antesera@users.noreply.github.com>
2022-11-29 02:44:02 -0500

    Add files via upload

---
+++ MethodCl/MethodCl/MethodClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodCl
+{
+    class MethodClass
+    {
+        //private int num1 { get; set; }
+        //private int num2 { get; set; }
+
+        int num1 = 4;
+        int num2 = 12;
+
+        public void OutPut(int number1, int number2)
+        {
+            int value = number2 / number1;
+        }
+        public void Output(int number1, int number2, out int result)
+        {
+            result = number1 / number2;
+        }
+    }
+    
+}
---
+++ MethodCl/MethodCl/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MethodCl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodClass methodClass = new MethodClass();
+
+            Console.WriteLine("12 / 5 = + value");
+            int value = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
+ 
