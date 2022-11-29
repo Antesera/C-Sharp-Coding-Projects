85de82b71d70d9a3167add3dd8154a200f6cde08
Antesera <107664612+Antesera@users.noreply.github.com>
2022-11-29 12:54:31 -0500

    Add files via upload

---
+++ MethodParameter/MethodParameter/parameter1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MethodParameter
+{
+    class parameter1
+    {
+        private int num1 { get; set; }
+        private int num2 { get; set; }
+
+        //declarative statement of the method
+
+        public void MethodParameter1()
+        {
+            
+        }
+        public int MethodParameter1( int num1, int num2)
+        {
+            
+            try
+            {
+                addTwoIntegers(num1, num2);
+            }
+            catch
+            {
+                addTwoIntegers(num1);
+               
+            }
+
+            int result = addTwoIntegers(num1, num2);
+            return result;
+        }
+
+        internal void add2int(int user1, int v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void add2ints(int user1)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int addTwoIntegers(int num1 ,int num2 = 0)
+        {
+            int sum = num1 + num2;
+            return sum;
+        }
+
+
+     }
+       
+}
