5ac2a32aeda7759e975ef89dc8217d8460b7dc48
Antesera <107664612+Antesera@users.noreply.github.com>
2022-11-29 09:40:20 -0500

    Add files via upload

---
+++ Exceptional Handling/Exceptional Handling/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exceptional_Handling
+{
+    class Program
+    {
+        private static int num0;
+        private static int num1;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> listnum = new List<int> ();              
+                
+                Console.WriteLine("please enter the first number ");
+                num0 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("please enter the second number ");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                foreach ( int i in listnum )
+                {
+                    Console.WriteLine(i/num0);
+                }          
+            }
+             catch(DivideByZeroException) //Division by zero attempt
+            {
+                Console.WriteLine("Don't divide by Zero");
+            }
+            catch(FormatException)//Catch block
+            {
+                Console.WriteLine("Enter value number");
+            }
+            finally 
+            {
+                Console.WriteLine("program Excuted");
+            }
+
+        }
+    }
+}
