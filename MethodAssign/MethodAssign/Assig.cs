1686a8e0b3921feb57a8f563739bcd1bef2b0b4c
Antesera <107664612+Antesera@users.noreply.github.com>
2022-11-28 23:59:49 -0500

    Add files via upload

---
+++ MethodAssign/MethodAssign/Assig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodAssign
+{
+    class Assig
+    {
+        private int num1;
+        private int num2;
+
+        public int Add(int num1, int num2)
+        {
+            int result = 0 + num2;
+            return num2;
+        }
+
+        public string MainMethod()
+        {
+            int result = num1 + num2;
+            return ("num1 + num2");
+            
+        }
+    }
+}
---
+++ MethodAssign/MethodAssign/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MethodAssign
+{
+    class Program
+    {
+        //private static string num1;
+        //private static string num2;
+
+        static void Main(string[] args)
+        {
+            MethodAssign.Assig X = new MethodAssign.Assig();
+            X.Add(num1: 12, num2: 13);
+
+            Console.WriteLine(Add(12, 13));
+            Console.WriteLine(Add(12));
+        }
+    }
+}
