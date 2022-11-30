6a350c21b9c7394934ffc3f080f136b98eb66d60
Antesera <107664612+Antesera@users.noreply.github.com>
2022-11-29 20:22:34 -0500

    Add files via upload

---
+++ MethodCl/MethodCl/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MethodCl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodClass methodClass = new MethodClass();
+            
+            Console.WriteLine(methodClass.OutPut(value));
+            Console.WriteLine("please type the numbers");
+        }
+    }
+}
+ 
