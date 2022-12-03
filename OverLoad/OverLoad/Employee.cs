f293a47c302357e29fc9a0e9185e6cb3f8d6327a
Antesera <107664612+Antesera@users.noreply.github.com>
2022-12-03 16:15:04 -0500

    Add files via upload

---
+++ OverLoad/OverLoad/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OverLoad
+{
+     class Employee : Pesron
+    {
+        public int ID { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Employee employee &&
+                   FirstName == employee.FirstName &&
+                   LastName == employee.LastName &&
+                   ID == employee.ID;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(FirstName, LastName, ID);
+        }
+
+        public static bool operator ==(Employee employee, Employee employee2)
+        {
+            if (employee.ID == employee2.ID)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //but you should use return employee.ID == employee2.ID;
+        public static bool operator !=(Employee employee, Employee employee2)
+        {
+            return employee.ID != employee2.ID;
+        }
+        
+     }
+}
---
+++ OverLoad/OverLoad/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OverLoad
+{
+    class Program   
+    {
+        static void Main(string[] args)
+        {
+            Employee Eg = new Employee();
+            Eg.FirstName = "Bela";
+            Eg.LastName = "Abera";
+            Eg.ID = 9112323;
+
+            Employee Eg1 = new Employee();
+            Eg.FirstName = "shashe";
+            Eg.LastName = "Mesfi";
+            Eg.ID = 3906054;
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
