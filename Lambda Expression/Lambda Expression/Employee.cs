18d050df3fb5d6405e554dba4f17b9dfaf313c04
Antesera <107664612+Antesera@users.noreply.github.com>
2022-12-07 20:34:02 -0500

    Add files via upload

---
+++ Lambda Expression/Lambda Expression/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lambda_Expression
+{
+
+    class Employee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        internal static void Add(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Lambda Expression/Lambda Expression/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lambda_Expression
+{
+    class Program
+    {
+        private static object employee;
+        public static int EmployeeID { get; set; }
+        public static int ID { get; private set; }
+        public static string FirstName { get; set; }
+        public static string LastName { get; set; }
+
+        static void Main(string[] args)
+        {
+            List<Employee> listEmployees = new List<Employee>()
+            {
+                new Employee{ID = 1,FirstName = "Bela", LastName = "Abera"},
+                new Employee{ID = 2,FirstName = "shashe",LastName = "Mesfi"},
+                new Employee{ID = 3,FirstName = "Joe", LastName = "Feta"},
+                new Employee{ID = 4,FirstName = "Joe", LastName = "Awoko"},
+                new Employee{ID = 5,FirstName = "Aman", LastName = "Tolla"},
+                new Employee{ID = 6,FirstName = "shibru", LastName = "Malto"},
+                new Employee{ID = 7,FirstName = "Sora", LastName = "Sobsa"},
+                new Employee{ID = 8,FirstName = "Animu", LastName = "Zewge"},
+                new Employee{ID = 9,FirstName = "Mamo", LastName = "Abi"},
+                new Employee{ID = 10,FirstName = "Tata", LastName = "Gebre"},
+            };
+
+            //List with each name of type Employee
+            List<Employee> EmpJoe = new List<Employee>();
+            Employee employee1 = listEmployees.Find(Employee => Employee.FirstName == FirstName);
+            Employee employee2 = listEmployees.Find(Employee => Employee.LastName == LastName);
+            foreach (Employee employees in listEmployees)
+            {
+                if (employees.FirstName == "Joe")
+                {
+                    EmpJoe.Add(employees);
+                    
+                }
+            }
+            Console.WriteLine($" Found(name) is the first name is the same");
+
+            //List with each Employee ID greater than  Five
+            List<Employee> EmpID = new List<Employee>();
+            Employee employee = listEmployees.Find(Employee => Employee.ID >= 5);
+            foreach (Employee employees in listEmployees)
+            {
+                if (employee.ID >=5)
+
+                {
+                    EmpID.Add(employee);
+                }
+            }
+            
+            Console.WriteLine($" Found(Name) is greater than Five");
+            Console.ReadLine();
+        }
+    }
+}
