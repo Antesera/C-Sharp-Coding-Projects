e469994283f795c7f88b6206368386426587732b
Antesera <107664612+Antesera@users.noreply.github.com>
2022-12-08 21:47:17 -0500

    Add files via upload

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime
+{
+    class DateTime
+    {
+        public static DateTime Now { get; set; }
+        public static object date02 { get;  set; }
+
+        static void Main(string[] args)
+        {
+            //DateTime properties
+            System.DateTime now = System.DateTime.Now;
+
+            //print current date and Time 
+            Console.WriteLine(now);
+            
+            Console.WriteLine("please enter the DateTime of next Tomorrow ?");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(now.AddHours(x));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DateTime/DateTime/now.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DateTime
+{
+    internal class now
+    {
+        public now()
+        {
+        }
+
+        public static implicit operator DateTimeKind(now v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
