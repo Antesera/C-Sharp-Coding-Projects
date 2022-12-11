349d5a5b77f84a46ed1c715f0c03485fc797ef21
Antesera <107664612+Antesera@users.noreply.github.com>
2022-12-11 14:24:26 -0500

    Add files via upload

---
+++ Construct Assign/Construct Assign/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Construct_Assign
+{
+   public class ConstructMe
+    {
+        public string name;
+        public int id;
+        public ConstructMe (string Name, int id)
+        {
+            this.name = Name;
+            this.id = id;
+        }
+    }
+    class Program
+    {
+        const int a = 10;
+        static void Main(string[] args)
+        {
+            ConstructMe a = new ConstructMe("Daniel", 28);
+            Console.WriteLine($"The instance with name: {nameof(a)}, has property name: {a.name} and id: {a.id}");
+            
+            Console.ReadKey();
+        }
+    }
+}
