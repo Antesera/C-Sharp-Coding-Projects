e02005c13cf364dab24e61b0128f36406a4dfb16
Antesera <107664612+Antesera@users.noreply.github.com>
2022-12-19 11:44:52 -0500

    Add files via upload

---
+++ Construct Assign/Construct Assign/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace Construct_Assign
+{
+    public class Bar
+    {
+        public int Amount { get; set; }
+
+        public Bar() :this(null, 0)
+        {
+        }
+
+        public Bar(int amount) :this(string.Empty, amount)
+        {
+
+        }
+
+        public Bar(string name, int amount) : base(name) 
+        {
+            this.Amount = amount;
+        }    
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First construct!");
+            Bar a = new Bar();
+            Console.WriteLine("Second construct!");
+            Bar b = new Bar();
+            Console.WriteLine("Third construct!");
+            Bar c = new Bar();
+
+            Console.ReadLine();
+        }
+    }
+}
