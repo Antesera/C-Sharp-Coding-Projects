182c9decb7b4ec08344dc8580df16ebd4dc21e1f
Antesera <107664612+Antesera@users.noreply.github.com>
2023-01-11 18:23:14 -0500

    Add files via upload

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -12,147 +12,114 @@ namespace ConsoleApp
 
         static void Main(string[] args)
         {
+            //part one 
             //one dimensional array
             //array using loops
             string[] students = { "Tom", "Victor", "Hakim", "Abti" };
 
-            Console.WriteLine("Enter students name to search for");
+            Console.WriteLine("please enter some text");//write the name to get string number
             string studentsName = Console.ReadLine();
 
-            bool contains = false;
+            
             for (int i = 0; i < students.Length; i++)
             {
-                if (studentsName == students[i])
-                {
-                    contains = true;
-                }
+                students[i] += studentsName;//write string number to describe the name of student
             }
-            if (contains == true)
+
+            for (int i = 0; i < students.Length; i++)
             {
-                Console.WriteLine("The student is in the class");
+                Console.WriteLine(students[i]);
             }
-            else
-            {
-                Console.WriteLine("The student is NOT in the class");
-            }
+
+            //part two
 
             //infinite loop
             int n = 11;
             do
             {
                 Console.WriteLine("Hello World " + n);
-                n++;
+                n++;//added n++ to make not infinite
             }
             while (n < 10);
 
-            for (int i = 0; i < 5; i++)
-            {
-                Console.Write(i + " ");
-            }
+
+            //part three
             //loop with operators
             for (int x = 1; x <= 6; x++)
             {
                 if (x == 5)
                     continue;
-                Console.WriteLine("Value is :{0}", x);
+                Console.WriteLine("Value is :{0}", x);//string  value of 0, lessthan or equal to six
             }
+            for (int x = 1; x < 6; x++)
+            {
+                if (x == 5)
+                    continue;
+                Console.WriteLine("Value is :{0}", x);//string  value of 0, lessthan six
+            }
+
+
+            //part four
             //unique list with loop
-            var duplicatedItems = new List<int>() { 1, 1, 2, 3, 3, 4, 5, 5, 5 };
-            var uniqueItems = duplicatedItems.Distinct();
-            foreach (var item in duplicatedItems)
+            var ustring = new List<string> { "apple", "orange", "bannana" };//unique item list 
+            Console.WriteLine("please enter a fruit");
+            var uniqueItems = Console.ReadLine();
+
+            for (int y = 0; y < ustring.Count; y++)
             {
-                Console.WriteLine(item);
+               if(ustring[y]== uniqueItems)
+               {
+                    Console.WriteLine(y);
+                    break;//stop and run the program
+               }
+                    
+            }
+            if (!ustring.Contains(uniqueItems))
+            {
+                Console.WriteLine("The item is not the list");
             }
 
+
+            //part five
             //create a list of values
-            List<int> myvalues = new List<int>() { 23, 34, 45, 56, 67 };
+            var ustringl = new List<string> { "apple", "orange", "bannana", "apple" };//duplicate item list
+            Console.WriteLine("please enter a fruit");
+            var items = Console.ReadLine();
 
-            //Declare the integer loop variable beforehand
-            int index = 0;
-
-            //loop variable with 1 each loop cycle 
-            foreach (int value in myvalues)
+            for (int y = 0; y < ustringl.Count; y++)
             {
-                Console.WriteLine($"{index}: {value}");
-                index++;
-            }
-
-            ConsoleKeyInfo cki;
-            cki = Console.ReadKey();
-            if (cki.Key == ConsoleKey.Y)
-            {
-                Console.Clear();
-            }
-            else if (cki.Key == ConsoleKey.N)
-            {
-                Console.Clear();
-            }
-            else
-            {
-                Console.WriteLine();
-                Console.WriteLine("Enter letters only");
-            }
-
-            // Part 1: two arrays are equal.
-            string[] array1 = { "cat", "bird" };
-            string[] array2 = { "cat", "bird" };
-            Console.WriteLine(array1.SequenceEqual(array2));
-
-            //all elements must be equal, no extra elements are allowed.
-            string[] array3 = { "cat", "bird", "frog" };
-            Console.WriteLine(array1.SequenceEqual(array3));
-
-            Console.Write("Enter a number\n");
-            int input = Convert.ToInt32(Console.ReadLine());// Convert to an integer
-
-            if (!Int32.IsNumber(input)) // if not a whole number input give  an error
-            {
-                Console.WriteLine("Not an integer");
-            }
-
-            List<string> numbers = new List<string> { "1", "2" };
-            List<string> numbers2 = new List<string> { "1", "2" };
-            if (numbers.Equals(numbers))
-            {
-                Console.WriteLine();
-            }
-
-            var listnames = new List<string> { "A", "B", "A" };
-
-            if (listNames.Distinct().Count() != listnames.Count())
-            {
-               Console.WriteLine("List contains duplicate value.");
-            }
-
-            Console.WriteLine("print array:");
-
-            //Creat an array
-            int[] a_array = new int[] { 1, 2, 3, 4, 5, 6, 1 };
-
-            //foreach loop begin it will run till the last element of the array
-            foreach(int items in a_array)
-            {
-                Console.WriteLine("which item are unique and duplicate");
-            }
-
-            //infinite loop
-            int count = 0;
-
-            while (true)
-            {
-                Console.WriteLine("Count = {0}.", count);
-                count++;
-
-                //...
-
-                if (count > 10)
+                if (ustringl[y] == items)
                 {
-
-                    break;
+                    Console.WriteLine(y);                  
                 }
+
+            }
+            if (!ustringl.Contains(items))//the items is not found in the list
+            {
+                Console.WriteLine("The item is not the list");
+            }
+
+
+            //part six
+            var ustring2 = new List<string> { "apple", "orange", "bannana", "apple" };//list of duplicate item
+            
+            List<string> ustring3 = new List<string>();
+
+
+            foreach (string x in ustring2)
+            {
+                if (ustring3.Contains(x))
+                {
+                    Console.WriteLine(x + " - this item is a duplicate");//out put of duplicate items
+
+                }
+                else
+                {
+                    Console.WriteLine(x + " - this item is a unique");//out put of unique items
+                }
+                ustring3.Add(x);
             }
             Console.ReadLine();
-
         }
     }
 }
